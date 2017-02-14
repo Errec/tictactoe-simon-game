d6327212863e03c54c13b78ffa8337290812163d
Raniro Coelho <errecme@gmail.com>
2017-02-14 17:56:17 -0200

    Rename tttManager argument to t

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-function tttManager(card, clickedSpot) {
-  if (card.playerXSpots.indexOf(clickedSpot) != -1 || card.playerOSpots.indexOf(clickedSpot) != -1) {
+function tttManager(t, clickedSpot) {
+  if (t.playerXSpots.indexOf(clickedSpot) != -1 || t.playerOSpots.indexOf(clickedSpot) != -1) {
     alert("item already checked"); // REMOVE
     return;
-  } else if (card.playerTurn === 'X') {
-    card.playerXSpots.push(clickedSpot);
+  } else if (t.playerTurn === 'X') {
+    t.playerXSpots.push(clickedSpot);
   } else {
-    card.playerOSpots.push(clickedSpot);
+    t.playerOSpots.push(clickedSpot);
   }
 
-  if (--card.openSpots === 0) {
+  if (--t.openSpots === 0) {
     alert("its a drawn"); // REMOVE
-    tttReset(card);
+    tttReset(t);
     return;
   }
-  console.log(card); // REMOVE
-  card.playerTurn === 'X' ? card.playerTurn = 'O' : card.playerTurn = 'X';
+  console.log(t); // REMOVE
+  t.playerTurn === 'X' ? t.playerTurn = 'O' : t.playerTurn = 'X';
 }
 
-function tttReset(card){
-  card.openSpots = 6;
-  card.playerTurn = 'X';
-  card.playerXSpots = [];
-  card.playerOSpots = [];
+function tttReset(t){
+  t.openSpots = 6;
+  t.playerTurn = 'X';
+  t.playerXSpots = [];
+  t.playerOSpots = [];
 }
 
 $(document).ready(function() {
-  var tttCard = {
+  var tttData = {
     openSpots    : 6,
     playerTurn   : 'X',
     playerXSpots : [],
@@ -34,7 +34,7 @@ $(document).ready(function() {
 
   $('.td').click(function() {
     if ($('.header__button').data('game') === 't') {
-      tttManager(tttCard, this.id);
+      tttManager(tttData, this.id);
     } else {
       alert('simon game!'); // REMOVE
     }
