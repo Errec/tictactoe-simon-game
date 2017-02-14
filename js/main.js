ee7a6e5f654634b984032dab5dc143accee52739
Raniro Coelho <errecme@gmail.com>
2017-02-14 20:59:59 -0200

    Add convert spot function This function converts a 4 points group in one single reference. E.g.: 11, 12, 21, 22 points are in the T1 group( Top 1).

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 function tttManager(t, clickedSpot) {
-  if (t.playerXSpots.indexOf(clickedSpot) != -1 || t.playerOSpots.indexOf(clickedSpot) != -1) {
+  var spotGroup = convertClickedSpot(clickedSpot);
+  if (t.playerXSpots.indexOf(spotGroup) != -1 || t.playerOSpots.indexOf(spotGroup) != -1) {
     alert("item already checked"); // REMOVE
     return;
   } else if (t.playerTurn === 'X') {
-    t.playerXSpots.push(clickedSpot);
+    t.playerXSpots.push(spotGroup);
   } else {
-    t.playerOSpots.push(clickedSpot);
+    t.playerOSpots.push(spotGroup);
   }
 
   if (--t.openSpots === 0) {
@@ -17,16 +18,66 @@ function tttManager(t, clickedSpot) {
   t.playerTurn === 'X' ? t.playerTurn = 'O' : t.playerTurn = 'X';
 }
 
-function tttReset(t){
-  t.openSpots = 6;
-  t.playerTurn = 'X';
+function convertClickedSpot(p) {
+  switch (p) {
+    case 'td-11':
+    case 'td-12':
+    case 'td-21':
+    case 'td-22':
+      return 'T1';
+    case 'td-13':
+    case 'td-14':
+    case 'td-23':
+    case 'td-24':
+      return 'T2';
+    case 'td-15':
+    case 'td-16':
+    case 'td-25':
+    case 'td-26':
+      return 'T3';
+    case 'td-31':
+    case 'td-32':
+    case 'td-41':
+    case 'td-42':
+      return 'M1';
+    case 'td-33':
+    case 'td-34':
+    case 'td-43':
+    case 'td-44':
+      return 'M2';
+    case 'td-35':
+    case 'td-36':
+    case 'td-45':
+    case 'td-46':
+      return 'M3';
+    case 'td-51':
+    case 'td-52':
+    case 'td-61':
+    case 'td-62':
+      return 'B1';
+    case 'td-53':
+    case 'td-54':
+    case 'td-63':
+    case 'td-64':
+      return 'B2';
+    case 'td-55':
+    case 'td-56':
+    case 'td-65':
+    case 'td-66':
+      return 'B3';
+  }
+}
+
+function tttReset(t) {
+  t.openSpots    = 9;
+  t.playerTurn   = 'X';
   t.playerXSpots = [];
   t.playerOSpots = [];
 }
 
 $(document).ready(function() {
   var tttData = {
-    openSpots    : 6,
+    openSpots    : 9,
     playerTurn   : 'X',
     playerXSpots : [],
     playerOSpots : []
